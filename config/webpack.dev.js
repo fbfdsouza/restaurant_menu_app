6e24b0f24acd6aedfa5ccc12375873c9a93a3844
fbfdsouza <fbfdsouza@fbfdsouzas-MacBook-Pro.local>
2020-08-30 18:17:23 -0300

    remove unnecessary files and fixing android issue building

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -10,7 +10,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "../www"),
-    publicPath: "/",
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
